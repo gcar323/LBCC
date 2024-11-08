bfc174fb123e52f9f4b0a49030e1da93208dab0a
gcar323 <gcarrerarosas3253@gmail.com>
2024-11-07 19:30:24 -0800

    LBCC

---
+++ schedule.py
@@ -0,0 +1,26 @@
+def calc2 ():
+    courses = [ [31207, "MOND/WEDN: 09:15 - 11:45"] , 
+                [31211, "TUES/THUR: 10:00 - 12:30"] ,
+                [31208, "MOND/WEDN: 12:00 - 14:30"] ,
+                [31206, "MOND/WEDN: 08:30 - 11:00"] ,
+                [31025, "TUES/THUR: 08:00 - 10:30"] ]
+
+def cs22 ():
+    courses = [ [31286, "TUES/THUR: 09:00 - 10:25"] , 
+                [31278, "SATURDAY : 09:00 - 12:10"] ,
+                [31285, "ASYNCHRONOUS"] ]
+
+def phys1 ():
+    courses = [ [32441, "TUES/THUR: 09:00 - 10:25"] , 
+                [32444, "SATURDAY : 09:00 - 12:10"] ,
+                [30234, "ASYNCHRONOUS"] ,
+                [30239, ] ,
+                [] ,
+                []]
+
+def engl3():
+    courses = [[] , [] , [] , [] , []]
+
+def main():
+    print("This school schedule haa to work with your work schedule.")
+
